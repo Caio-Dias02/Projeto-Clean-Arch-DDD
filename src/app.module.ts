66dcfd92052f9c3b37cf0d78f29aa58d68c9241e
Caio <caiodias9713@gmail.com>
2025-08-13 09:08:26 -0300

    Update project configuration and dependencies. Added Prettier settings for code formatting, integrated @nestjs/config for environment configuration, and updated package dependencies in package.json and package-lock.json.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { EnvConfigModule } from './shared/insfrastructure/env-config/env-config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
